21d5e4873720bec486bf437cb11b8c83de925c54
kolasniwash <nickashaw3@gmail.com>
2023-12-13 21:09:23 +0000

    solution d4 rust both parts

---
+++ rust/aoc2015/day-4/src/main.rs
@@ -0,0 +1,17 @@
+use md5;
+
+fn main(){
+    let key = String::from("yzbqklnj");
+    let mut index = 0;
+    let mut no_match_found = true;
+    while no_match_found {
+        let secret_key = format!("{}{}", key, index);
+        let ref_hash = md5::compute(secret_key.as_bytes());
+        let format_hash = format!("{x:x}", x=ref_hash);
+
+        if &format_hash[0..5] == "00000" {
+            no_match_found = false;
+            println!("{:?}", index)
+        } else { index +=1; }
+    }
+}
