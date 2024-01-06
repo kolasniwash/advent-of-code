754b580e1bc7bc9ee993cd473c8af5244eccaec8
kolasniwash <nickashaw3@gmail.com>
2024-01-06 12:15:25 +0100

    part2 solution day6

--- rust/aoc2015/day-6/src/main.rs
+++ rust/aoc2015/day-6/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 enum Action {
     On,
     Off,
-    Toggle
+    Toggle,
 }
 
 #[derive(Debug)]
@@ -13,67 +13,79 @@ struct Instruction {
     x_start: i32,
     y_start: i32,
     x_end: i32,
-    y_end: i32
+    y_end: i32,
 }
 
 impl Instruction {
     fn new(action: Action, x_start: i32, y_start: i32, x_end: i32, y_end: i32) -> Instruction {
-        Instruction{ action, x_start, y_start, x_end, y_end }
+        Instruction {
+            action,
+            x_start,
+            y_start,
+            x_end,
+            y_end,
+        }
     }
 }
 
-fn parse_instruction(instruction: &str) -> Instruction{
-    let instruction_parts:Vec<&str> = instruction.split(",").collect();
-
-    let first_part: Vec<&str> = instruction_parts[0].split("turn").map(|x| x.trim()).collect();
-    let first_part_split: Vec<&str> = first_part[first_part.len()-1].split(" ").collect();
-
-    let action: Action = match first_part_split[0]{
-        "on" => {Action::On},
-        "off" => {Action::Off},
-        "toggle" => {Action::Toggle},
-        _ => {Action::On}
-    };
-
-    let x_start: i32 = first_part_split[1].parse().expect("Failed to convert to int");
-
-    let second_part: Vec<&str> = instruction_parts[1].split(" ").collect();
-
-    let y_start: i32 = second_part[0].parse().expect("Failed to convert to int");
-    let x_end: i32 = second_part[second_part.len()-1].parse().expect("Failed to convert to int");
-
-    let y_end:i32 = instruction_parts[instruction_parts.len()-1].parse().expect("Failed to convert to int");
-
-    println!("{:?} x:{} y:{}  x_end:{} y_end:{}", action, x_start, y_start, x_end, y_end);
-    Instruction{
-        action,
-        x_start,
-        y_start,
-        x_end,
-        y_end
-    }
-
-}
-
-fn main(){
-    let instructions = fs::read_to_string("/Users/nshaw/Code/kolasniwash/advent-of-code/rust/aoc2015/day-6/src/input.txt")
-        .expect("File not found");
-
-    let mut lights: Vec<Vec<i32>> = Vec::with_capacity(1000);
-    for _ in 0..1000 {
-        lights.push(vec![0; 1000]);
-    }
-    // println!("{:?}", lights);
-
+fn parse_instructions(instructions: String) -> Vec<Instruction> {
+    let mut parsed_instructions: Vec<Instruction> = Vec::new();
     for instruction in instructions.split("\n") {
-        println!("{}", instruction);
-        let parsed_instruction: Instruction = parse_instruction(instruction);
+        let instruction_parts: Vec<&str> = instruction.split(",").collect();
 
-        for row in parsed_instruction.x_start..parsed_instruction.x_end+1 {
-            for col in parsed_instruction.y_start..parsed_instruction.y_end+1{
+        let first_part: Vec<&str> = instruction_parts[0]
+            .split("turn")
+            .map(|x| x.trim())
+            .collect();
+        let first_part_split: Vec<&str> = first_part[first_part.len() - 1].split(" ").collect();
+
+        let action: Action = match first_part_split[0] {
+            "on" => Action::On,
+            "off" => Action::Off,
+            "toggle" => Action::Toggle,
+            _ => Action::On,
+        };
+
+        let x_start: i32 = first_part_split[1]
+            .parse()
+            .expect("Failed to convert to int");
+
+        let second_part: Vec<&str> = instruction_parts[1].split(" ").collect();
+
+        let y_start: i32 = second_part[0].parse().expect("Failed to convert to int");
+        let x_end: i32 = second_part[second_part.len() - 1]
+            .parse()
+            .expect("Failed to convert to int");
+
+        let y_end: i32 = instruction_parts[instruction_parts.len() - 1]
+            .parse()
+            .expect("Failed to convert to int");
+
+        println!(
+            "{:?} x:{} y:{}  x_end:{} y_end:{}",
+            action, x_start, y_start, x_end, y_end
+        );
+        parsed_instructions.push(Instruction {
+            action,
+            x_start,
+            y_start,
+            x_end,
+            y_end,
+        })
+    }
+    parsed_instructions
+}
+
+fn part_1_solution(
+    mut lights: Vec<Vec<i32>>,
+    parsed_instructions: Vec<Instruction>,
+) -> Vec<Vec<i32>> {
+    for parsed_instruction in parsed_instructions.iter() {
+        for row in parsed_instruction.x_start..parsed_instruction.x_end + 1 {
+            for col in parsed_instruction.y_start..parsed_instruction.y_end + 1 {
                 match parsed_instruction.action {
-                    Action::On => { lights[row as usize][col as usize] = 1}
-                    Action::Off => { lights[row as usize][col as usize] = 0}
+                    Action::On => lights[row as usize][col as usize] = 1,
+                    Action::Off => lights[row as usize][col as usize] = 0,
                     Action::Toggle => {
                         if 0 == lights[row as usize][col as usize] {
                             lights[row as usize][col as usize] = 1
@@ -85,9 +97,49 @@ fn main(){
             }
         }
     }
+    lights
+}
+
+fn part_2_solution(
+    mut lights: Vec<Vec<i32>>,
+    parsed_instructions: Vec<Instruction>,
+) -> Vec<Vec<i32>> {
+    for parsed_instruction in parsed_instructions.iter() {
+        for row in parsed_instruction.x_start..parsed_instruction.x_end + 1 {
+            for col in parsed_instruction.y_start..parsed_instruction.y_end + 1 {
+                match parsed_instruction.action {
+                    Action::On => lights[row as usize][col as usize] += 1,
+                    Action::Off => {
+                        if 0 < lights[row as usize][col as usize] {
+                            lights[row as usize][col as usize] -= 1
+                        }
+                    }
+                    Action::Toggle => lights[row as usize][col as usize] += 2,
+                }
+            }
+        }
+    }
+    lights
+}
+
+fn main() {
+    let instructions = fs::read_to_string(
+        "/Users/nshaw/Code/kolasniwash/advent-of-code/rust/aoc2015/day-6/src/input.txt",
+    )
+    .expect("File not found");
+
+    let parsed_instructions: Vec<Instruction> = parse_instructions(instructions);
+
+    let mut lights: Vec<Vec<i32>> = Vec::with_capacity(1000);
+    // lights = lights.iter().map(|x| x.push(vec![0; 1000])).collect();
+    for _ in 0..1000 {
+        lights.push(vec![0; 1000]);
+    }
+
+    // lights = part_1_solution(lights, parsed_instructions);
+    lights = part_2_solution(lights, parsed_instructions);
 
     let total_on: i32 = lights.iter().map(|x| x.iter().sum::<i32>()).sum();
 
-    // let total_on: i32 = lights.into_iter().map(|row| row.into_iter().sum::<u32>().collect()).sum();
     println!("Total lights on: {}", total_on)
 }
